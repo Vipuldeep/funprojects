c752a9ccc500621e3f8f3aa19ff54e0c7eb14bc8
Vipul <vipuldeep95@gmail.com>
2021-03-22 13:04:38 +1000

    links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">  
+      <div class="page-example">
+        <a href="https://aagenda.netlify.app/" target="_blank" rel="noreferrer">Agenda<br/></a>
+        <a href="https://minimalistic-resume-1.netlify.app/" target="_blank" rel="noreferrer">Resume Template<br/></a>
+        <a href="https://www.deliciousdelightsbypriti.com/" target="_blank" rel="noreferrer">Delicious delights<br/></a>
+        <a href="https://www.thisisvipul.com/" target="_blank" rel="noreferrer">portfolio<br/></a>
+      </div>
     </div>
   );
 }
