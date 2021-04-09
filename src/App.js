470a2caf688ee435c9e620f39d4ee56d86844aec
Vipul <vipuldeep95@gmail.com>
2021-04-09 15:19:50 +1000

    changes to the link

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   return (
     <div className="App">  
       <div class="page-example">
+        <p className="heading">fun projects</p>
         <a href="https://aagenda.netlify.app/" target="_blank" rel="noreferrer">Agenda<br/></a>
         <a href="https://minimalistic-resume-1.netlify.app/" target="_blank" rel="noreferrer">Resume Template<br/></a>
-        <a href="https://www.deliciousdelightsbypriti.com/" target="_blank" rel="noreferrer">Delicious delights<br/></a>
-        <a href="https://www.thisisvipul.com/" target="_blank" rel="noreferrer">portfolio<br/></a>
+        <a href="https://randomnamegeneratorapi.netlify.app/" target="_blank" rel="noreferrer">Random Name Generator<br/></a>
+        <a href="https://github.com/Vipuldeep/nightcrawler/" target="_blank" rel="noreferrer">VS Code dark <br/></a>
       </div>
     </div>
   );
