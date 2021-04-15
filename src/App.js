17a579b7fa1b5ff06276f5cdd6d4a17d5fa44216
Vipul <vipuldeep95@gmail.com>
2021-04-15 11:05:46 +1000

    added links and readme

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ function App() {
   return (
     <div className="App">  
       <div class="page-example">
-        <p className="heading">fun projects</p>
         <a href="https://aagenda.netlify.app/" target="_blank" rel="noreferrer">Agenda<br/></a>
         <a href="https://minimalistic-resume-1.netlify.app/" target="_blank" rel="noreferrer">Resume Template<br/></a>
-        <a href="https://randomnamegeneratorapi.netlify.app/" target="_blank" rel="noreferrer">Random Name Generator<br/></a>
         <a href="https://github.com/Vipuldeep/nightcrawler/" target="_blank" rel="noreferrer">VS Code dark <br/></a>
+        <a href="https://cryptocurrencyanalyser.netlify.app/" target="_blank" rel="noreferrer">Cryptocurrency Analyser <br/></a>
+        <a href="https://randomnamegeneratorapi.netlify.app/" target="_blank" rel="noreferrer">Random Name Generator<br/></a>
       </div>
     </div>
   );
